30615093beb139b4e37fc02b73b16f87b63bcf11
Guitarist-Tolkienist <guitarist.tolkienist@gmail.com>
2021-05-08 15:54:11 +0300

    - [MATH] Added overloaded operators to work with 2d Vectors

--- Sources/MATH.cpp
+++ Sources/MATH.cpp
@@ -4,33 +4,91 @@
 namespace _2D {
 
     SVector_2D::SVector_2D() :
-            m_X(0),
-            m_Y(0) {}
+            X(0),
+            Y(0) {}
 
     SVector_2D::SVector_2D(float x, float y) :
-            m_X(x),
-            m_Y(y) {}
+            X(x),
+            Y(y) {}
 
     SVector_2D::SVector_2D(const SVector_2D& Vector_2D) {
-        m_X = Vector_2D.m_X;
-        m_Y = Vector_2D.m_Y;
+        X = Vector_2D.X;
+        Y = Vector_2D.Y;
     }
 
     // OPERATORS
-    void SVector_2D::operator+=(const SVector_2D &Vector2) {
-        this->m_X += Vector2.m_X;
-        this->m_Y += Vector2.m_Y;
-    }
-
-    void SVector_2D::operator-=(const SVector_2D &Vector2) {
-        this->m_X -= Vector2.m_X;
-        this->m_Y -= Vector2.m_Y;
-    }
-
     SVector_2D& SVector_2D::operator=(const SVector_2D &Vector2) {
-        this->m_X = Vector2.m_X;
-        this->m_Y = Vector2.m_Y;
+        this->X = Vector2.X;
+        this->Y = Vector2.Y;
         return *this;
     }
 
+    SVector_2D& SVector_2D::operator+=(const SVector_2D &Vector2) {
+        this->X += Vector2.X;
+        this->Y += Vector2.Y;
+        return *this;
+    }
+    SVector_2D& SVector_2D::operator-=(const SVector_2D &Vector2) {
+        this->X -= Vector2.X;
+        this->Y -= Vector2.Y;
+        return *this;
+    }
+    SVector_2D& SVector_2D::operator*=(float Value) {
+        this->X *= Value;
+        this->Y *= Value;
+
+        return *this;
+    }
+    SVector_2D& SVector_2D::operator/=(float Value) {
+        this->X /= Value;
+        this->Y /= Value;
+
+        return *this;
+    }
+
+    SVector_2D operator+(const SVector_2D& Vector1, const SVector_2D& Vector2) {
+        return SVector_2D(Vector1.X + Vector2.X, Vector1.Y + Vector2.Y);
+    }
+    SVector_2D operator-(const SVector_2D& Vector1, const SVector_2D& Vector2) {
+        return SVector_2D(Vector1.X - Vector2.X, Vector1.Y - Vector2.Y);
+    }
+
+    SVector_2D operator*(const SVector_2D &Vector1, float Value) {
+        return SVector_2D(Vector1.X * Value, Vector1.Y * Value);
+    }
+    SVector_2D operator*(float Value, const SVector_2D &Vector1) {
+        return Vector1 * Value;
+    }
+
+    std::ostream& operator<<(std::ostream& out, const SVector_2D& Vector) {
+        out << "Vector (" << Vector.X << ", " << Vector.Y << ")\n";
+        return out;
+    }
+
+
+    // RECTANGLE
+    bool DoRectanglesOverlap(const SRectangle& R1, const SRectangle& R2) {
+        if (DoRectanglesOverlap_X(R1, R2) &&
+            DoRectanglesOverlap_Y(R1, R2)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    bool DoRectanglesOverlap_X(const SRectangle& R1, const SRectangle& R2) {
+        if ((R1.TopLeft.X < R2.BottomRight.X  ) && (R1.BottomRight.X > R2.TopLeft.X  )) {
+            return true;
+        }
+
+        return false;
+    }
+
+    bool DoRectanglesOverlap_Y(const SRectangle& R1, const SRectangle& R2) {
+        if ((R1.TopLeft.Y < R2.BottomRight.Y  ) &&
+            (R1.BottomRight.Y > R2.TopLeft.Y  ) ) {
+            return true;
+        }
+        return false;
+    }
 }
